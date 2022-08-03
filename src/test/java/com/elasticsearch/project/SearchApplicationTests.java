9ab79319844cb378999b3484f33e752a796289af
slrslrr2 <slrslrr1@gmail.com>
2022-08-04 00:03:14 +0900

    first commit

---
+++ src/test/java/com/elasticsearch/project/SearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
